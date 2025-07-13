adf387878cd31048af3931bea6d2c3b58de7afe3
jack <xilio1024@gmail.com>
2025-07-13 23:57:40 +0800

    feat: logback日志

---
+++ mouse-logger/src/main/java/cn/xilio/mouse/cloud/logger/LoggerApplication.java
@@ -0,0 +1,17 @@
+package cn.xilio.mouse.cloud.logger;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerApplication {
+    @Value("${server.port}")
+    private static String port;
+    private static Logger logger = org.slf4j.LoggerFactory.getLogger(LoggerApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(LoggerApplication.class, args);
+        logger.debug("已经启动服务器{}",port);
+    }
+}
